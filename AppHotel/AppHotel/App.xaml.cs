c506b75f5e6bdf0960f25cd7913066d474f24395
Eduardo Frasson <edudomine2016@gmail.com>
2022-08-24 20:57:39 -0300

    Adicionar arquivos de projeto.

---
+++ AppHotel/AppHotel/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using AppHotel.View;
+
+namespace AppHotel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new View.Login();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AppHotel/AppHotel/Model/Suite.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppHotel.Model
+{
+    public class Suite
+    {
+        public string Nome { get; set; }
+        public double DiariaAdulta { get; set; }
+
+        public double DiariaCrianca { get; set; }
+    }
+}
